a53a2e1583e139d6322355ea6e80e1beea971b32
Michael Ganger <michael@michael-laptop.workgroup>
2014-02-20 16:24:29 -0500

    commited again

---
+++ slave/slave.cc
@@ -0,0 +1,28 @@
+// This is the code for the slave (balloon).
+//
+//
+//
+
+
+#include <dataPoint.h>
+
+struct dataStruct
+{
+  int bid;
+  int 
+  
+  
+}
+void setup()
+{
+
+}
+
+void loop()
+{
+  getData();
+  getPic();
+  writeData();
+  borp();
+  counter();
+}
