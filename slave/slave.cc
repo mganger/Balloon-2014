e87f8129d86961328bfd996afb6d8a1b2d633c15
Michael Ganger <michael@michael-laptop.workgroup>
2014-02-22 11:57:54 -0500

    updated the data structs. There is now dataSrtuct and dataTran

--- slave/slave.cc
+++ slave/slave.cc
@@ -4,18 +4,34 @@
 //
 
 
-#include <dataPoint.h>
+//#include <dataPoint.h>
+//#include <arduino.h>
+#include <stdio.h>
+//#include <MD5.h>
 
-struct dataStruct()
+struct trans
 {
-  unsigned int bid;
-  unsigned int temp;
-  unsigned int pressure;
-  unsigned int co2;
-  unsigned int n2;
-  unsigned int picture[];
-  unsigned int time;  
-}
+	int transferIndex;
+	int transferTime;
+	short unsigned int errorCode;		//0 - 255, Might include corrupted transmission, no data to send, low battery, etc. 0 = ok
+	
+} dataTran, picturesTran;
+
+
+struct data			//this gets written to the SD
+{
+	int bid;
+	int priority;
+	unsigned long int dataIndex;		//this is how the data is indexed. 32 bits, so keep below 4 billion points
+	int temp;		
+	int pressure;
+	int co2;
+	int n2;
+//	int picture[300];	//this is to store the picture as a series of 16 bit ints
+	int picName;		//Picture file name
+//	int picIndex;		//index to recompile the picture in order
+	int dataTime;  		
+} dataStruct;
 
 void setup()
 {
@@ -24,9 +40,9 @@ void setup()
 
 void loop()
 {
-  getData();
-  getPic();
-  writeData();
-  borp();
-  counter();
+//	getData();
+//	getPic();
+//	writeData();
+//	borp();
+//	counter();
 }
