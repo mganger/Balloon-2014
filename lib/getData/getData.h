21e85cf56c4a53c36d672e15374854245e08716d
aredman <aredman314@gmail.com>
2014-03-07 16:07:15 -0500

    Deleted lots of unecessary folders. Moved borp to Borp.

--- lib/getData/getData.h
+++
@@ -1,5 +0,0 @@
-//Function that gets the required data from the sensors and puts it into
-//the structure passed to it.
-//Should include millis() to write time into data collection, so that we
-//can record the collection time.
-
