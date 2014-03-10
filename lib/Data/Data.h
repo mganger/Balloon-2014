4a362cf47e3805dd62d3b2c4de1fd46106523bfe
Michael Ganger <mganger747@gmail.com>
2014-03-10 19:16:44 -0400

    added gpl to Data.h

--- lib/Data/Data.h
+++ lib/Data/Data.h
@@ -1,5 +1,24 @@
-//Class for holding individual points of Data
-//Created by Andrew Redman on 03/07/2014
+/*
+ * Data.h
+ * This file is part of Borp
+ *
+ * Copyright (C) 2014 - Houghton College Science Honors
+ *
+ * Borp is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Borp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Borp. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 
 #include <getData.h>
 
