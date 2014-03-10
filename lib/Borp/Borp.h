261ffb83c0c49f9234d06a7b04b2dafa3f6ff55e
Michael Ganger <mganger747@gmail.com>
2014-03-10 19:18:04 -0400

    added gpl to borp.h

--- lib/Borp/Borp.h
+++ lib/Borp/Borp.h
@@ -1,7 +1,25 @@
 /*
-Borp.h - Library for sending data from an Arduino over a serial connection, specifically designed for the Arudino Due and MM2 radio chips.
-Balloon Over Radio Protocol (B.O.R.P)
-Created by Hougthon College Science Honors program 2014
+* Borp.h - Library for sending data from an Arduino over a serial connection, specifically designed for the Arudino Due and MM2 radio chips.
+* Balloon Over Radio Protocol (B.O.R.P)
+* Created by Hougthon College Science Honors program 2014
+* 
+* Borp.h
+* This file is part of <program name>
+*  Copyright (C) 2014 - Michael Ganger
+*  <program name> is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*  <program name> is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*  You should have received a copy of the GNU General Public License
+* along with <program name>. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
 */
 
 #ifndef Borp_h		//Checks to see if this library has been included already
