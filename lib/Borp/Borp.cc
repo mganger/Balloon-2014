dc63d2fe71334d435bbcbde06516fd458deb0e6d
Michael Ganger <mganger747@gmail.com>
2014-03-10 19:20:46 -0400

    added borp to borp.cc

--- lib/Borp/Borp.cc
+++ lib/Borp/Borp.cc
@@ -1,8 +1,29 @@
 /*
-Borp.cc - Library for sending data from an Arduino over a serial connection, specifically developed for the Arduino Due and MM2 radio chips.
-Balloon Over Radio Protocl (B.O.R.P)
-Created by Houghton College Science Honors program 2014
-*/
+ * Borp.cc - Library for sending data from an Arduino over a serial connection,
+ * specifically developed for the Arduino Due and MM2 radio chips.
+ * Balloon Over Radio Protocl (B.O.R.P)
+ * Created by Houghton College Science Honors program 2014
+ *
+ * Borp.cc
+ * This file is part of Borp
+ *
+ * Copyright (C) 2014 - Houghton College Science Honors
+ *
+ * Borp is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Borp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Borp. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ 
 
 #include "Arduino.h" 		//Includes all the standard Arduino Libraries
 #include "borp.h"		//Includes header file for B.O.R.P. Libraries
