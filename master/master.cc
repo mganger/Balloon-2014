a976d56fbb0b523b2a4d432ad4e0addafd3c4271
aredman <aredman314@gmail.com>
2014-03-14 19:11:50 -0400

    moved dependencies into GPL comment.

--- master/master.cc
+++ master/master.cc
@@ -16,8 +16,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Borp. If not, see <http://www.gnu.org/licenses/>.
- */
-/*Current Dependencies:
+ *
+ * Current Dependencies:
  *	openssl headers (openssl-devel on fedora)
  */
 
