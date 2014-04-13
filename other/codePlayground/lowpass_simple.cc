4af0c80923cdf7c701f9746ad4f94402cfa87af5
Michael Ganger <mganger747@gmail.com>
2014-04-13 11:56:01 -0400

    Updated the lowpass filter to use long doublle precision. Outputs 16 digits for each input.

--- other/codePlayground/lowpass_simple.cc
+++ other/codePlayground/lowpass_simple.cc
@@ -3,6 +3,10 @@
 //from http://phrogz.net/js/framerate-independent-low-pass-filter.html
 //Usage ./lowpass <amount> <file>
 //10 is probably good enough for the amount
+//This has been updated to use long double floating point precision (80 bits)
+//The algorithm is so fast that we might as well be as precise as possible
+//The longest part is literally writing to the file
+//Note that this leads to about 5 times data expansion (.6 orders of magnitude)
 
 #include <fstream>
 #include <iostream>
@@ -10,6 +14,7 @@
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
+#include <iomanip>					//for setprecision() for outfile
 
 using namespace std;
 
@@ -38,6 +43,30 @@ int* atoiArray(stringstream& streamer, int* lineCount){
 	return intConvert;
 }
 
+long double* atofArray(stringstream& streamer, int* lineCount){
+	
+	//Go through the string and count the number of newlines
+	cout << "Counting lines" << endl;
+	string lineString = streamer.str();
+	const char * lineChar = lineString.c_str();
+	for(unsigned long int i = streamer.str().length(); i; i--){
+		if(lineChar[i] == '\n'){
+			lineCount[0]++;
+		}
+	}
+	//Make a new array of that size
+	cout << "Converting to ints" << endl;
+	long double* doubleConvert = new long double[lineCount[0]];
+	//convert the string line by line into that
+	for(int i = 0; i < lineCount[0]; i++){
+		string buff;
+		getline(streamer, buff);
+		doubleConvert[i] = atof(buff.c_str());
+	}
+
+	return doubleConvert;
+}
+
 void lowpass(int * array, int size, int b){
 
 	//do it forward and backward to avoid phase issues
@@ -56,6 +85,25 @@ void lowpass(int * array, int size, int b){
 	return;
 }
 
+void lowpass(long double * array, int size, double b){
+
+	//do it forward and backward to avoid phase issues
+	long double oldvalue = array[0];
+	cout << "Pass 1" << endl;
+	for(int i = 0; i < size; i++){
+		oldvalue += (array[i] - oldvalue) / b;
+		array[i] = oldvalue;
+	}
+	cout << "Pass 2" << endl;
+	oldvalue = array[size];
+	for(int i = size; i == 0; i--){
+		oldvalue += (array[i] - oldvalue) / b;
+		array[i] = oldvalue;
+	}
+	return;
+}
+
+
 int main(int argc, char ** argv){
 	if(argc != 3){
 		cerr << "Wrong number of arguments" << endl;	
@@ -69,12 +117,13 @@ int main(int argc, char ** argv){
 	file.close();
 
 	int lineCount = 0;
-	int* array = atoiArray(fileStr, &lineCount);
+	long double* array = atofArray(fileStr, &lineCount);
 
-	lowpass(array, lineCount, atoi(argv[1]));
+	lowpass(array, lineCount, atof(argv[1]));
 
 	cout << "Writing to file" << endl;
 	ofstream outfile;
+	outfile << setprecision(16);
 	outfile.open("smooth.csv");
 	for (int i = 0; i < lineCount; i++){
 		outfile << array[i] << endl;
