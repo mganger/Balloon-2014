1b2db713df8c5aec9c449a3b9ea19e7b52935655
Michael Ganger <mganger747@gmail.com>
2014-05-15 19:16:30 -0400

    Working on the program for interrupts. Added TimerOne libraries to test their utility effectiveness

--- other/codePlayground/interrupt/src/interrupt.ino
+++ other/codePlayground/interrupt/src/interrupt.ino
@@ -3,23 +3,54 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include "TimerOne.h"
  
-#define LEDPIN 2
+#define LEDPIN 13
+
+//global variable to switch the pin
+bool switcheroo = HIGH;
+
+void switchPin(){
+
+	digitalWrite(LEDPIN,switcheroo);
+	switcheroo = !switcheroo;
+
+}
 
 void setup(){
-	pinMode(LEDPIN, OUTPUT);
 
-	// initialize Timer1
-	cli();             // disable global interrupts
-	TCCR1A = 0;        // set entire TCCR1A register to 0
-	TCCR1B = 0;
+	pinMode(LEDPIN, OUTPUT);
+	//setup the timer and interrupts
+	Serial.begin(115200);
+	Timer1.initialize(1000000); //Call interrupt every 1000ms
+	Timer1.attachInterrupt(switchPin);
+	Timer1.start();
+
+
+
+
+//	//initialization blinking
+//	for(int i = 0; i<1000; i++){
+//		digitalWrite(LEDPIN,switcheroo);
+//		switcheroo = !switcheroo;
+//		delay(100);
+//	}
+
+	//other way to change the pin output value
+//	DDRB = B10000000;
+//	PORTB = B11111111;
+
+	//// initialize Timer1
+	//cli();             // disable global interrupts
+	//TCCR1A = 0;        // set entire TCCR1A register to 0
+	//TCCR1B = 0;
  
 	// enable Timer1 overflow interrupt:
-	TIMSK1 = (1 << TOIE1);
+	//TIMSK1 = (1 << TOIE1);
 	// Set CS10 bit so timer runs at clock speed:
-	TCCR1B |= (1 << CS10);
+	//TCCR1B |= (1 << CS10);
 	// enable global interrupts:
-	sei();
+	//sei();
 
 
 }
