641586056d9b51eb3cbe4fed312d4d5d9417d913
aredman <aredman314@gmail.com>
2014-05-28 09:03:24 -0400

    removed serial.available because 1. it caused problems. 2. it was unnecessary

--- slave/src/Borp.cpp
+++ slave/src/Borp.cpp
@@ -41,16 +41,14 @@ Borp::Borp(){
 
 //Manually writes the long int array as ascii to the hardware serial
 void Borp::broadcast(unsigned long int * dataArray,int size){
-	if(Serial.available()){
-//		for(int i = 1; i < size; i++ ){
-//				int num = dataArray[i];
-//				char tmp[10] = {0,0,0,0,0,0,0,0,0,0};
-//			for(int h = size;h >=0 ;h--){
-//				tmp[h] = num % 10;
-//				num /= 10;
-//			}
-//		}
-	}
+		for(int i = 1; i < size; i++ ){
+				int num = dataArray[i];
+				char tmp[10] = {0,0,0,0,0,0,0,0,0,0};
+			for(int h = size;h >=0 ;h--){
+				tmp[h] = num % 10;
+				num /= 10;
+			}
+		}
 }
 
 bool Borp::compareArrays(char * array1, char* array2, int size){
