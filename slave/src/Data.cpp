f816703793a88e9a5690de66d0a3b108dc3e957d
aredman <aredman314@gmail.com>
2014-05-28 10:23:42 -0400

    ste debugging

--- slave/src/Data.cpp
+++ slave/src/Data.cpp
@@ -80,7 +80,6 @@ Data::Data(){
 	}
 	memset(dataArray,INIT,SIZE*4);
 	Serial.println("Initialized Array");
-	printData();
 	dataArray[INDEX] = dataArray[INDEX] +1;
 	//initialize the pressure sensor
 }
