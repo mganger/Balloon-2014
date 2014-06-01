b5cac819dcdbb31deda0627a41629183c88c1564
Michael Ganger <mganger747@gmail.com>
2014-05-31 21:33:25 -0400

    irrelevant liveplot and gnuscript pushes

--- master/src/liveplot.sh
+++ master/src/liveplot.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-
+echo "This script only works on files already on the disk. It does not work on devices. Use picocom and tee for that."
+echo "Takes 3 files as argument"
 read -s -p "Enter password for remote server: " remotepass
 
-if [[ ! -f $1 ]]
-then
-	echo "Using $1"
-else
-	echo "$1 is not a device"
-	exit
-fi
-
-#./master.cc $ 115200
 
 while :
 	do
-	
-	#mkdir ./graphs
+	./fileparse $1
+	./fileparse $2
+	./fileparse $3
+
+	mkdir -p ./graphs
 	gnuplot -persist -e "load 'gnuscript'"
 	
 	sshpass -p $remotepass scp graphs/*.svg pi@ganger.webhop.org:/var/www/science_honors/
